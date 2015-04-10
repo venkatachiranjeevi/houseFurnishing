820e980a2c326da244c599cbfe12436c89cf71b3
venkatachiranjeevi <kokku2chiranjeevi@gmail.com>
2015-04-10 19:22:26 +0530

    intern apps

--- static/js/imageLoad.js
+++ static/js/imageLoad.js
@@ -1,4 +1,4 @@
-var item=null,allCoords=[],houseDetails=[];
+var item=null,allCoords=[],houseDetails=[],index= 0,radius=50;
 window.onload=function() {
     var stage = new Kinetic.Stage({
             container: 'container',
@@ -11,9 +11,16 @@ window.onload=function() {
     $(function() {
         $("#room-name").change(function(event){
             roomDisplay();
-
+            index=allCoords.length-1;
+             for(var k=0;k<allCoords.length;k++){
+                if(allCoords[k][0].name==$("#room-name").val()){
+                    index=k;
+                    break;
+                }
+            }
         });
     });
+
     $(function() {
         $(".wall-prop").change(function(event){
             drawRoom();
@@ -21,6 +28,7 @@ window.onload=function() {
         });
 
     });
+
     function roomDisplay(){
         var flag=0;
         if ($("#room-name").val()!="") {
@@ -32,11 +40,7 @@ window.onload=function() {
                     break;
                 }
             }
-            //fix1Room();
             if (flag == 0) {
-                var x = document.getElementsByClassName("wall-prop");
-                //for (var i = 0; i < x.length; i++)
-                //    x[i].disabled = false;
                 allCoords[allCoords.length]=new Array();
                 allCoords[allCoords.length-1].push({name:$("#room-name").val(),wallAfeet:$("#wall-a-feet").val(),wallAInch:$("#wall-a-inches").val(),wallBfeet:$("#wall-b-feet").val(),wallBInch:$("#wall-b-inches").val()});
             }
@@ -45,8 +49,10 @@ window.onload=function() {
             layer.removeChildren();
         }
     }
+
     function drawRoom() {
         layer.removeChildren();
+        designLayer.removeChildren();
         if($("#room-name").val()!="") {
             var background = new Kinetic.Rect({
                 x: 52,
@@ -63,17 +69,19 @@ window.onload=function() {
             stage.add(layer);
         }
     }
+
     function displayRoom(temp){
         for(var k=1;k<temp.length;k++){
-            if(temp[k].name!="sofa") {
+            if(temp[k].name!="sofa"&&temp[k].name!="bed") {
               drawObjects(temp[k]);
             }
             else{
                 drawImages(temp[k])
             }
         }
-        stage.add(layer);
+
     }
+
     function drawLines(x, y) {
         var text = "";
         var wallA1 = new Kinetic.Line({
@@ -159,90 +167,83 @@ window.onload=function() {
     function getWidth() {
         return parseInt($("#wall-a-feet").val()) * 11 + parseInt($("#wall-a-inches").val());
     }
+
     function getHeight() {
         return parseInt($("#wall-b-feet").val()) * 11 + parseInt($("#wall-b-inches").val());
     }
 
     var con=stage.getContainer();
-    document.getElementById("door").addEventListener('dragstart',function(e){
+    $("#door")[0].addEventListener('dragstart',function(e){
         item=this;
     });
-    document.getElementById("window").addEventListener('dragstart',function(e){
+    $("#window")[0].addEventListener('dragstart',function(e){
         item=this;
     });
-    document.getElementById("wardrobe").addEventListener('dragstart',function(e){
+    $("#wardrobe")[0].addEventListener('dragstart',function(e){
         item=this;
     });
-    document.getElementById("sofa").addEventListener('dragstart',function(e){
+    $("#sofa")[0].addEventListener('dragstart',function(e){
         item=this;
     });
-    document.getElementById("cooking-area").addEventListener('dragstart',function(e){
+    $("#cooking-area")[0].addEventListener('dragstart',function(e){
         item=this;
     });
-    document.getElementById("plumbing").addEventListener('dragstart',function(e){
+    $("#plumbing")[0].addEventListener('dragstart',function(e){
         item=this;
     });
-    document.getElementById("bed").addEventListener('dragstart',function(e){
+    $("#bed")[0].addEventListener('dragstart',function(e){
         item=this;
     });
-    document.getElementById("plug").addEventListener('dragstart',function(e){
+    $("#plug")[0].addEventListener('dragstart',function(e){
         item=this;
     });
+
     con.addEventListener('dragover',function(e){
         e.preventDefault();
     });
+
     con.addEventListener('drop',function(e){
-         e.preventDefault();
-        var index=allCoords.length-1;
-        var details=null,radius;
+        e.preventDefault();
+        var rects=stage.find(".rectangle");
+        var details=null;
         if($("#room-name").val()!="") {
             var color = "", xpos= e.layerX, ypos= e.layerY;
             if (item.id == "door") {
                 color = "#B8860B";
-                radius=60;
             }
             else if (item.id == "window") {
                 color = "#ADD8E6";
-                radius = 60;
             }
             else if (item.id == "wardrobe") {
                 color = "#FFEBCD";
-                radius=40;
             }
             else if (item.id == "cooking-area") {
                 color = "#6B92C8";
-                radius=40;
-            }
-            else if (item.id == "bed") {
-                color = "#FAFBF5";
-                radius=50;
             }
             else if (item.id == "plumbing") {
                 color = "#5D7F8C";
-                radius=50;
             }
             else if (item.id == "plug") {
                 color = "black";
-                radius=100;
+                radius=radius+50;
             }
-            for(var k=0;k<allCoords.length;k++){
-                if(allCoords[k][0].name==$("#room-name").val()){
-                    index=k;
-                    break;
-                }
-            }
-            if(item.id!="sofa")
+
+            if((item.id!=="sofa"&&item.id!="bed") ) {
                 details=getRectValues(e.layerX, e.layerY,item.id,radius,color);
-            if(item.id!=="sofa" && details!=0) {
                 drawObjects(details);
             }
-            else if(item.id=="sofa"){
+            else if(item.id=="sofa"||item.id=="bed"){
+                var length=getWidth()-20,height=getHeight()-40;
+                if(item.id=="bed"){
+                    height=height+60;
+                    length=length-15;
+                }
                 details={
-                    len:80,
-                    ht:60,
-                    xpos: get_Sofa_X(e.layerX),
-                    ypos: get_Sofa_Y(e.layerY),
-                    name:'sofa',
+                    len:length,
+                    ht:height,
+                    xpos: get_SofaBed_X(e.layerX,length,height),
+                    ypos: get_SofaBed_Y(e.layerY,length,height),
+                    name:item.id,
                     color:color,
                     wallLen:getWidth(),
                     wallHeight:getHeight()
@@ -252,24 +253,33 @@ window.onload=function() {
             allCoords[index].push(details);
         }
         else{
-            alert("Fix The Room First");
+            alert("Choose room first");
         }
     });
+
     function drawObjects(details){
         var diffX=getWidth()-details.wallLen;
         var diffY=getHeight()-details.wallHeight;
-        details.xpos+=diffX;
-        details.ypos+=diffY;
         details.wallLen=getWidth();
         details.wallHeight=getHeight();
+        details.xpos=details.xpos+diffX;
+        details.ypos=details.ypos+diffY;
+        if(details.len==10){
+            details.ht+=diffY;
+        }
+        else if(details.ht==10){
+            details.len+=diffX;
+        }
+        var group=new Kinetic.Group({
 
-        var background = new Kinetic.Rect({
+        });
+        var furniture = new Kinetic.Rect({
             x: details.xpos,
             y: details.ypos,
             width: details.len,
             height: details.ht,
             fill: details.color,
-            name: 'furniture',
+            name: 'rectangle',
             draggable: true,
             dragBoundFunc: function (pos) {
                 var X = pos.x;
@@ -285,27 +295,34 @@ window.onload=function() {
                 });
             }
         });
-        layer.add(background);
-        stage.add(layer);
+        group.add(furniture);
+        designLayer.add(group);
+        stage.add(designLayer);
         return details;
     }
+
     function drawImages(details){
         var diffX=getWidth()-details.wallLen;
         var diffY=getHeight()-details.wallHeight;
+        details.len+=diffX;
+        details.ht+=diffX;
         details.xpos+=diffX;
         details.ypos+=diffY;
         details.wallLen=getWidth();
         details.wallHeight=getHeight();
+        var group=new Kinetic.Group({
+        });
         var image=new Kinetic.Image({
-            x: details.xpos,
-            y: details.ypos,
+            x: get_SofaBed_X(details.xpos,details.len,details.ht),
+            y: get_SofaBed_Y(details.ypos,details.len,details.ht),
             width:details.len,
             height:details.ht,
             name:details.name,
+            id:item.id,
             draggable:true,
             dragBoundFunc: function(pos) {
-                var X=get_Sofa_X(pos.x);
-                var Y=get_Sofa_Y(pos.y);
+                var X=get_SofaBed_X(pos.x,details.len,details.ht);
+                var Y=get_SofaBed_Y(pos.y,details.len,details.ht);
                 details.xpos=X;
                 details.ypos=Y;
                 return ({
@@ -313,35 +330,92 @@ window.onload=function() {
                     y:Y
                 });
             }
-
         });
-        layer.add(image);
-        imgObj=new Image();
-        imgObj.src="static/sf1.png";
-        imgObj.onload=function(){
-            image.setImage(imgObj)
-            layer.draw()
-        };
-        stage.add(layer);
+        group.add(image);
+        designLayer.add(group);
+        if(details.name=="sofa") {
+            imgObj=new Image();
+            imgObj.src = "static/sf1.png";
+            imgObj.onload=function(){
+                image.setImage(imgObj)
+                designLayer.draw()
+            };
+        }
+        else if(details.name=="bed") {
+            imgObj1=new Image();
+            imgObj1.src = "static/bed-image.png";
+            imgObj1.onload=function(){
+                image.setImage(imgObj1)
+                designLayer.draw()
+            };
+        }
+        deleteIcon(details,group);
+        designLayer.add(group);
+        stage.add(designLayer);
+        group.on('mouseover',function(){
+             document.body.style.cursor = 'pointer';
+            stage.find("#"+image.id())[0].fill("#E3E3EC").opacity(0.4);
+            stage.find("#close-icon")[0].show();
+            stage.find("#close-icon")[0].on("click", function(){
+                deleteItem(image.id,designLayer);
+            })
+            designLayer.draw()
+        });
+        group.on('mouseout', function() {
+        document.body.style.cursor = 'default';
+             stage.find("#"+image.id())[0].fill(null).opacity(1);
+            stage.find("#close-icon")[0].show().hide();
+            designLayer.draw()
+      });
         return details;
     }
-    function get_Sofa_X(val){
+    function deleteIcon(details,group){
+        var x=stage.find("#close-icon")[0];
+        if(x===undefined)
+            alert("sdf");
+        //stage.remove(x);
+        //layer.draw();
+        var img=new Kinetic.Image({
+             x:details.xpos,
+             y:details.ypos,
+             width:15,
+             height:15,
+             id:"close-icon",
+             visible:false
+         });
+        delIcon=new Image();
+        delIcon.src="static/close-icon.png";
+        delIcon.onload=function(){
+            img.setImage(delIcon)
+            designLayer.draw()
+        };
+        group.add(img);
+
+    }
+    function deleteItem(unit,layer){
+            var it=stage.find("#"+unit)[0]
+            stage.remove(it);
+            layer.draw();
+    }
+    function get_SofaBed_X(val,itemLen,itemHt){
         if(val<59)
             return 59;
-        if(val>350+getWidth()-35)
-            return 350+getWidth()-35;
+        if(val+itemLen>350+getWidth())
+            return 350+getWidth()+45-itemLen;
         return val;
     }
-    function get_Sofa_Y(val){
+
+    function get_SofaBed_Y(val,itemLen,itemHt){
         if(val<52)
            return 52;
-        if(val>250+getHeight()-26)
-            return 250+getHeight()-26;
+        if(val+itemHt>250+getHeight())
+            return 250+getHeight()+35-itemHt;
         return val;
     }
+
     function getRectValues(x,y,name,r,color){
         var xp=x,yp= y,wN,width=getWidth()- r,height=getHeight()-r;
-        if((x >= 43 && x <= ( 350 + getWidth() + 30))&&((y>25&&y<60)||(y>250+getHeight()-5&&y<250+getHeight()+50))){
+        if((x >= 43 && x <= ( 350 + getWidth() + 25))&&((y>20&&y<65)||(y>250+getHeight()-10&&y<250+getHeight()+50))){
             if(y>25&&y<60) {
                 yp = 38;
                 wN="WallA";
@@ -359,14 +433,15 @@ window.onload=function() {
                 ht:10,
                 xpos:xp,
                 ypos:yp,
-                name:name,
+                name:'rectangle',
+                id:name,
                 color:color,
                 wallName:wN,
                 wallLen:getWidth(),
                 wallHeight:getHeight()
             };
         }
-        else if(((x>25&&x<=53)||(x>350+getWidth()+30&&x<350+getWidth()+60))&&(y>=25&&y<=250+getHeight()+40)){
+        else if(((x>20&&x<=53)||(x>350+getWidth()+25&&x<350+getWidth()+65))&&(y>=30&&y<=250+getHeight()+45)){
             if(x>25&&x<=53) {
                 xp = 48;
                 wN="WallD"
@@ -380,11 +455,12 @@ window.onload=function() {
             if(yp+r>250+getHeight()+13)
                 yp = 300 + r;
             return{
-                len:8,
+                len:10,
                 ht:getHeight()-r,
                 xpos:xp,
                 ypos:yp,
-                name:name,
+                name:'rectangle',
+                id:name,
                 color:color,
                 wallName:wN,
                 wallLen:getWidth(),
@@ -396,6 +472,7 @@ window.onload=function() {
             return 0;
         }
     }
+
     function get_X_Y(wall,x,y,width,height){
         if(wall=="WallA"){
             if(x<46)
@@ -427,13 +504,14 @@ window.onload=function() {
         }
         return [x,y];
     }
+
     $(function(){
        $("#estimator").click(function(event){
            event.preventDefault();
         if(allCoords.length!=0) {
             for(var i=0;i<allCoords.length;i++){
                 if(allCoords[i][0].name.indexOf("Bedroom")>-1||allCoords[i][0].name.indexOf("Kitchen")>-1){
-                    customizeRoom(allCoords[i]);
+                    customizeRoom(allCoords[i],allCoords[i][0].name);
                 }
             }
             render2html(houseDetails);
@@ -444,19 +522,19 @@ window.onload=function() {
        });
     });
 
-    function customizeRoom(temp){
+    function customizeRoom(temp,roomName){
         var roomDetails=[];
         var roomlen;
         var wallAC=parseInt(temp[0].wallAfeet)*11+parseInt(temp[0].wallAInch);
         var wallBD=parseInt(temp[0].wallBfeet)*11+parseInt(temp[0].wallBInch);
         for(var j=1;j<temp.length;j++){
-            if(temp[j].name=="wardrobe"||temp[j].name=="cooking-area"){
+            if((temp[j].id=="wardrobe"&&roomName.indexOf("Bedroom")>-1)||(temp[j].id=="cooking-area" && roomName.indexOf("Kitchen")>-1)){
                 roomlen=(temp[j].ht==10)?wallAC:wallBD;
                 for(var k=0;k<3;k++){
                     space=((roomlen-1)/11-k).toFixed(2);
                     roomDetails.push({
                         RoomLength:space,
-                        itemName:temp[j].name,
+                        itemName:temp[j].id,
                         itemLength:space-1,
                         itemCost:getCost(space,temp[j].name)
                     });
@@ -468,12 +546,14 @@ window.onload=function() {
             value:roomDetails
         });
     }
+
     function getCost(feet,item){
         if(item=="wardrobe")
             return "Rs. "+((feet*6*800).toFixed(2))
         else
             return "Rs. "+(feet*5900).toFixed(2)
     }
+
     function render2html(roomsObjects){
         event.preventDefault();
         var roomDetails=JSON.stringify(roomsObjects);
